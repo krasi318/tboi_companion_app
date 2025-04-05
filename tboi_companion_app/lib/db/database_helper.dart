f676cb3270e37d835d2682b1d03100ce67fcb9e9
Krasi <kkrrasii@gmail.com>
2025-04-05 13:48:29 +0300

    Adding the app structure
    App structure
    todo - fix the library with all the items
    implement search function for the items
    and most importantly implement function to scan items on another screen and recognise what item is shown and show its description

---
+++ tboi_companion_app/lib/db/database_helper.dart
@@ -0,0 +1,49 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import '../models/item.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper instance = DatabaseHelper._init();
+  static Database? _database;
+
+  DatabaseHelper._init();
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDB('all_items.db');
+    return _database!;
+  }
+
+  Future<Database> _initDB(String fileName) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, fileName);
+
+    return await openDatabase(path, version: 1, onCreate: _createDB);
+  }
+
+  Future _createDB(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE items (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        description TEXT,
+        imagePath TEXT
+      )
+    ''');
+  }
+
+  Future<void> insertItem(Item item) async {
+    final db = await instance.database;
+    await db.insert(
+      'items',
+      item.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List<Item>> getAllItems() async {
+    final db = await instance.database;
+    final result = await db.query('items');
+    return result.map((e) => Item.fromMap(e)).toList();
+  }
+}
---
+++ tboi_companion_app/lib/scan_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ScanScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF1E1E1E),
+      appBar: AppBar(
+        title: Text('Scan Item'),
+        backgroundColor: Color(0xFF2C2C2C),
+      ),
+      body: Center(
+        child: Text(
+          'Scan feature coming soon...',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
